5c8206d9b235a95b969d363c28326de6328c5a04
MeLJes <meljesx@gmail.com>
2018-01-26 15:41:45 +0200

    getCommentsByAuthorId() done

--- task-1-2/script.js
+++ task-1-2/script.js
@@ -1,52 +1,10 @@
 // Sort by year
-// function sortByYear() {
-//     var filmsArr = filmsInJSON.slice(0);
-//
-//     filmsArr.sort(function(a, b) {
-//         return a.year - b.year;
-//     });
-//     console.log('Sort by year:', filmsArr);
-// }
-//sortByYear();
-
-
-// Sort by year
 function sortByYear(films) {
     return films.sort(function(a, b) {
         return a.year - b.year;
     });
 }
 //sortByYear(filmsInJSON);
-
-
-// Filter by years
-// function filterByYears(array, minYear, maxYear) {
-//     var filmsArr = filmsInJSON.slice(0);
-//
-//     // Undefined properties
-//     minYear = minYear || undefined || null;
-//     maxYear = maxYear || undefined;
-//
-//     if( array.length && minYear === undefined && maxYear === undefined ) {
-//         console.log('Full films list:', filmsArr);
-//     } else if ( array.length && minYear && minYear !== undefined && minYear !== null && minYear !== 'null' && maxYear === undefined ) {
-//         var filmsWithMinYear =  filmsArr.filter(function (entry) {
-//             return entry.year >= minYear;
-//         });
-//         console.log('In the range from min year:', filmsWithMinYear);
-//     } else if ( minYear === null || minYear === 'null' ) {
-//         var filmsWithMaxYearOnly = filmsArr.filter(function (entry) {
-//             return entry.year <= maxYear;
-//         });
-//         console.log('In the range up to the maximum date:', filmsWithMaxYearOnly);
-//     } else if ( array.length && maxYear && maxYear !== undefined ) {
-//         var filmsWithMaxYear = filmsArr.filter(function (entry) {
-//             return entry.year >= minYear && entry.year <= maxYear;
-//         });
-//         console.log('In the range from the min to the max date:', filmsWithMaxYear);
-//     }
-// }
-//filterByYears('array', 'null', 2000);
 
 
 // Filter by years
@@ -125,36 +83,8 @@ function getAmountByGenres(films) {
 
 
 // Get total duration
-// function getTotalDuration() {
-//     var filmsArr = filmsInJSON.slice(0);
-//     var holder = [];
-//
-//     // Sum function
-//     function getSum(a, b) {
-//         return a + b;
-//     }
-//
-//     // Get all durations
-//     var getTotalDuration = filmsArr.forEach(function (entry) {
-//         holder.push(entry.duration);
-//     });
-//
-//     // Get sum
-//     var totalDuration = holder.reduce(getSum);
-//
-//     console.log('Total duration:', totalDuration);
-// }
-//getTotalDuration();
-
-
-// Get total duration
 function getTotalDuration(films) {
     var holder = [];
-
-    // Sum function
-    function getSum(a, b) {
-        return a + b;
-    }
 
     // Get all durations
     films.forEach(function (film) {
@@ -162,14 +92,24 @@ function getTotalDuration(films) {
     });
 
     // Get sum
-    return holder.reduce(getSum);
+    return holder.reduce(function (a, b) {
+        return a + b;
+    });
 }
 //getTotalDuration(filmsInJSON);
 
 
 // Get total comments by film
 function getTotalCommentsByFilm(films, filmId) {
-    return films[filmId].comments.length;
+    var holder;
+
+    films.forEach(function (film) {
+        if( film.id === filmId ) {
+            holder = film.comments.length;
+        }
+    });
+
+    return holder;
 }
 //getTotalCommentsByFilm(filmsInJSON, 4);
 
@@ -179,12 +119,13 @@ function getCommentsByAuthorId(films, authorId) {
     var holder = [];
 
     films.forEach(function (film) {
-        //holder.push(film.comments[authorId].text);
-
-        holder.push(film.comments[[authorId].text]);
+        film.comments.forEach(function (comment) {
+            if( comment.authorId === authorId ) {
+                holder.push(comment);
+            }
+        });
     });
 
     return holder;
 }
 //getCommentsByAuthorId(filmsInJSON, 1001);
-console.log( getCommentsByAuthorId(filmsInJSON, 1001) );
