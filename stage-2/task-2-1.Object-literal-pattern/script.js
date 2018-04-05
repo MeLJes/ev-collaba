949e27aec85f80c515fa3da93681c44ba04c1ef7
MeLJes <meljesx@gmail.com>
2018-04-05 16:52:49 +0300

    Update task

--- stage-2/task-2-1.Object-literal-pattern/script.js
+++ stage-2/task-2-1.Object-literal-pattern/script.js
@@ -1,25 +1,38 @@
 (function () {
 	let people = {
-		people: ['will', 'laura'],
+		people: [],
 		init: function() {
-			this.controlElements();
+			this.cashElements();
+			this.bindEvents();
 			this.render();
 		},
-		controlElements: function() {
-			this.formInput = document.getElementsByClassName('.personName'),
-			this.formButton = document.getElementsByClassName('.addPerson'),
-			this.persons = document.getElementsByClassName('persons'),
-			this.template = this.persons.innerHTML
+		cashElements: function() {
+			this.formInput = document.querySelector('.personName');
+			this.formButton = document.querySelector('.addPerson');
+			this.persons = document.querySelector('.persons');
+			this.personRemove = document.querySelector('.remove');
+		},
+		bindEvents: function() {
+			this.formButton.addEventListener('click', this.addPerson.bind(this));
+			this.formButton.addEventListener('click', this.removePerson.bind(this));
 		},
 		render: function() {
-			let data = this.people.map(function(person) {
-				return '<li>' + person + '<button type="button" class="remove"></button></li>';
-			});
+			let personsList = this.people.map(function(person) {
+				return `<li class="person">
+					${person}
+					<button type="button" class="remove"></button>
+				</li>`;
+			}).join('');
 
-			this.template = data
+			this.persons.innerHTML = personsList;
+		},
+		addPerson: function() {
+			this.people.push(this.formInput.value);
+			this.render();
+			this.formInput.value = '';
+		},
+		removePerson: function () {
 
-			console.log(this);
-			console.log(this.people);
 		}
 	};
 
