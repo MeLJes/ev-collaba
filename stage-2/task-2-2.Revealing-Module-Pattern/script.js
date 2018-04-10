98e11b3f9fb4df14bff4a3abb0c4fcd3702c59fb
MeLJes <meljesx@gmail.com>
2018-04-10 15:32:51 +0300

    Second task complete

--- stage-2/task-2-2.Revealing-Module-Pattern/script.js
+++ stage-2/task-2-2.Revealing-Module-Pattern/script.js
@@ -1,50 +1,46 @@
-(function () {
-	let people = {
-		people: [],
-		init: function() {
-			this.cacheElements();
-			this.bindEvents();
-			this.render();
-		},
-		cacheElements: function() {
-			this.formInput = document.querySelector('.personName');
-			this.formButton = document.querySelector('.addPerson');
-			this.persons = document.querySelector('.persons');
-		},
-		bindEvents: function() {
-			this.formButton.addEventListener('click', this.addPerson.bind(this));
-			this.persons.addEventListener('click', this.removePerson.bind(this));
-		},
-		render: function() {
-			let personsList = this.people.map(function(person) {
-				return `<li class="person">
+let people = (function () {
+	let people = [],
+			formInput = document.querySelector('.personName');
+			formButton = document.querySelector('.addPerson');
+			persons = document.querySelector('.persons');
+
+	formButton.addEventListener('click', addPerson);
+	persons.addEventListener('click', removePerson);
+
+	render();
+
+	function render() {
+		let personsList = people.map(function (person) {
+			return `<li class="person">
 					${person}
 					<button type="button" class="remove"></button>
 				</li>`;
-			}).join('');
+		}).join('');
 
-			this.persons.innerHTML = personsList;
-		},
-		addPerson: function() {
-			if (this.formInput.value.length) {
-				this.people.push(this.formInput.value);
-				this.render();
-				this.formInput.value = '';
-			}
-		},
-		removePerson: function (event) {
-			if (event.target && event.target.classList.contains('remove')) {
-				let item = event.target.closest('li');
-				let personaName = item.innerText.trim();
+		persons.innerHTML = personsList;
+	}
+	function addPerson(person) {
+		var name = (formInput.value.length && typeof person === 'string')
+								? person
+								: formInput.value;
+		people.push(name);
+		render();
+		formInput.value = '';
+	}
+	function removePerson(event) {
+		if (event.target && event.target.classList.contains('remove')) {
+			let item = event.target.closest('li');
+			let personaName = item.innerText.trim();
 
-				this.people = this.people.filter(function(person) {
-					return person !== personaName;
-				});
-				this.render();
-			}
-
+			people = people.filter(function (person) {
+				return person !== personaName;
+			});
+			render();
 		}
-	};
+	}
 
-	people.init();
+	return {
+		addPerson: addPerson,
+		removePerson: removePerson
+	}
 })();
