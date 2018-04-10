0523c0d1d2efadfe0fbd1cf11ebe54e0c73f5b71
MeLJes <meljesx@gmail.com>
2018-04-10 15:35:18 +0300

    Add Task-2-3

---
+++ stage-2/task-2-3.PubSub-Pattern/script.js
@@ -0,0 +1,46 @@
+let people = (function () {
+	let people = [],
+			formInput = document.querySelector('.personName');
+			formButton = document.querySelector('.addPerson');
+			persons = document.querySelector('.persons');
+
+	formButton.addEventListener('click', addPerson);
+	persons.addEventListener('click', removePerson);
+
+	render();
+
+	function render() {
+		let personsList = people.map(function (person) {
+			return `<li class="person">
+					${person}
+					<button type="button" class="remove"></button>
+				</li>`;
+		}).join('');
+
+		persons.innerHTML = personsList;
+	}
+	function addPerson(person) {
+		var name = (formInput.value.length && typeof person === 'string')
+								? person
+								: formInput.value;
+		people.push(name);
+		render();
+		formInput.value = '';
+	}
+	function removePerson(event) {
+		if (event.target && event.target.classList.contains('remove')) {
+			let item = event.target.closest('li');
+			let personaName = item.innerText.trim();
+
+			people = people.filter(function (person) {
+				return person !== personaName;
+			});
+			render();
+		}
+	}
+
+	return {
+		addPerson: addPerson,
+		removePerson: removePerson
+	}
+})();
